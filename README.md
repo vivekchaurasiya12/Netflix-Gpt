ca68dd7d82d8b1fe4f169d1423ba946047510cef
vivekchaurasiya12 <vivekchaurasiyatutorials@gmail.com>
2025-02-21 05:35:34 +0530

    tailwind and project setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# React + Vite
+
+# Features
+ - Login/Singup 
+   - after login user redirect to browse page
+ - Browse Page(after authentication)
+     - header
+     - main movie
+       - trailer in background
+       - title and description
+       - movie suggestions
+         - movie-list
+ - Netflix-gpt
+   - search bar
+   - movie suggestions
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Hello, Vite + React + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
