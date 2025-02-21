f6ff58604591d5f7ec6837dd2a832c5732c91717
vivekchaurasiya12 <132990592+vivekchaurasiya12@users.noreply.github.com>
2025-02-21 08:26:04 +0530

    Sign in form completed

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter,RouterProvider } from "react-router-dom";
 import Browse from "./Browse";
 import Login from "./Login";
-
+import Header from "./Header";
 const Body = ()=>{
     // HERE WE HAVE DONE THE WHOLE ROUTING PART FOR THIS PROJECT
     const appRouter  = createBrowserRouter([
@@ -9,6 +9,10 @@ const Body = ()=>{
             path: "/",
             element:<Login/>
         },
+        {
+          path: "/header",
+          element:<Header/>
+      },
         {
             path: "/browse",
             element:<Browse/>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
+
+import Header from "./Header";
 const Login =()=>{
+  const [signin,setSignin] = useState(true);
+  const handleSignIn=()=>{
+    setSignin(!signin);
+  }
     return(
-      <h1>this is login page</h1>
+      <div className="relative h-screen">
+        <Header/>
+        <div>
+        <img src="
+https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg" alt="Netflix Background" className="w-full min-h-screen"/>
+      </div>
+      {/*  form  */}
+        <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-50 px-12 py-14 rounded-lg">
+          <h1 className="text-2xl text-white font-bold">{signin ? "Sign In":"Sign Up" }</h1>
+          <div className="flex flex-col items-center">
+          <input type="email" placeholder="Email" className="w-96 mt-5 p-2 rounded-md" />
+          <input type="password" placeholder="Password" className="w-96 mt-5 p-2 rounded-md" />
+          <button className="w-96 mt-5 p-2 bg-red-600 text-white rounded-md">Sign In</button>
+          </div>
+          {/* <div className="mt-5 text-white">
+            <input type="checkbox" />
+            <span className="ml-2">Remember Me</span>
+            <span className="float-right">Need Help?</span>
+          </div> */}
+          <div className="mt-5 text-white">
+            <span>{signin ?"New to Netflix?": "Already existing customer?" } </span>
+            <span className="text-red-600 ml-2 cursor-pointer" onClick={handleSignIn}>{signin ?"Sign Up Now": "Sign In" }</span>
+          </div>
+        </form>
+     
+      </div>
     )
 }
 export default Login;
